4a46808d751d7d075ad2e2b04da977e7cb7e7194
rafaelborattobomfim-sys <rafael.boratto.bomfim@escola.pr.gov.br>
2026-08-14 09:54:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// Seleciona todos os botões do blog
+const botoes = document.querySelectorAll("button");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    
+    botao.addEventListener("click", function() {
+        console.log("Post interagido!");
+        let contador = botao.querySelector("span");
+        
+        if (curtiu === false) {
+            contador.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#27ae60"; // Fica verde quando clicado
+            botao.style.color = "black";
+        } else {
+            contador.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "#000000"; // Volta ao preto
+            botao.style.color = "white";
+        }
+    });
+});
